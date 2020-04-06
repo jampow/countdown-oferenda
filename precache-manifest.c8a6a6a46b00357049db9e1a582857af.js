c9c2734b620ff2a3a75754f2f9974cf57118a96d
Gianpaulo M. Soares <jam_pow@hotmail.com>
2020-04-05 22:28:56 -0300

    Updates

---
+++ precache-manifest.c8a6a6a46b00357049db9e1a582857af.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f497ba27e3d82d52f4b",
+    "url": "/countdown-oferenda/static/js/main.5f497ba2.chunk.js"
+  },
+  {
+    "revision": "9db3ed95da66445b4996",
+    "url": "/countdown-oferenda/static/js/1.9db3ed95.chunk.js"
+  },
+  {
+    "revision": "3d18e9f10e2b7b2e8abc",
+    "url": "/countdown-oferenda/static/js/runtime~main.3d18e9f1.js"
+  },
+  {
+    "revision": "781532ac33e7f2a1fb886d77e6537641",
+    "url": "/countdown-oferenda/index.html"
+  }
+];
